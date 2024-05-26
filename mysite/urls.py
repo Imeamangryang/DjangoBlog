3eb36b76c8d74da5d22f05358b90fdb82d14798f
Imeamangryang <imeamangryang@naver.com>
2024-05-27 02:26:55 +0900

    update blog post

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
+    path(r'admin/', admin.site.urls),
     path(r'', include('blog.urls')),
 ]
